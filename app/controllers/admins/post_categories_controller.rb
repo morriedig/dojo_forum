bd5a95a384f959a55320e26b56de7b246158c74f
Morrie <morrie@4point-inc.com>
2018-07-29 16:03:04 +0800

    add jquery and post_category can create and delete by ajax and remove test file

---
+++ app/controllers/admins/post_categories_controller.rb
@@ -0,0 +1,29 @@
+class Admins::PostCategoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @post_categories = PostCategory.all
+    @post_category = PostCategory.new
+  end
+
+  def create
+    @post_category = PostCategory.create(post_category_params)
+    # 之後要寫判斷失敗的狀況
+  end
+
+  def destroy
+    find_post_category
+    @post_category.destroy
+  end
+
+  private
+
+  def find_post_category
+    @post_category = PostCategory.find(params[:id])
+  end
+
+  def post_category_params
+    params.require(:post_category).permit(:title)
+  end
+  
+end
