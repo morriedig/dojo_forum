bf80f5238395635b41bc48e0d229451a3f4b8748
Morrie <morrie@4point-inc.com>
2018-09-09 17:40:17 +0800

    fix again

--- db/migrate/20180729101527_remove_category_id_from_posts.rb
+++ db/migrate/20180729101527_remove_category_id_from_posts.rb
@@ -1,6 +1,6 @@
 class RemoveCategoryIdFromPosts < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :posts, :post_category, index: true
+    # remove_reference :posts, :post_category, index: true
     # remove_foreign_key :copies, :post_categories
     # remove_reference :posts, :post_category_id, foreign_key: true
   end
