5f5098ab1ddda60fdd198194ad179e0ac9c69654
morrie <morrie@morriede-MacBook-Pro.local>
2018-10-08 03:50:38 +0800

    add post viewed permission and fix most_viewed link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,31 +2,46 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    post_permission = lambda do |post|
+      return true if post.user === current_user
+      return true if post.post_permission === 1
+      return true if current_user && post.post_permission === 2 && post.user.friended.include?(current_user)
+      return false
+    end
+
     @post_categories = PostCategory.includes(:posts, :posts => [:user, :loved_users]).where(:posts => { post_state: "publish" })
     if params[:category_id]
       @post_category = PostCategory.includes(:posts => [:user] ).find(params[:category_id])
       if params[:sort] == "most_reply"
-        instance_variable_set("@post#{@post_category.id}", @post_category.posts.order_most_reply.includes(:user, :replies, :post_categories, :collection_posts).page(params[:page]).per(20) )
-      end
-      if params[:sort] == "latest_reply"
-        instance_variable_set("@post#{@post_category.id}", @post_category.posts.order_latest_reply.includes(:user, :replies, :post_categories, :collection_posts).page(params[:page]).per(20) )
+        instance_variable_set("@post#{@post_category.id}", Kaminari.paginate_array( @post_category.posts.order_most_reply.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
+      elsif params[:sort] == "latest_reply"
+        instance_variable_set("@post#{@post_category.id}", Kaminari.paginate_array( @post_category.posts.order_latest_reply.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
+      elsif params[:sort] == "most_viewed"
+        instance_variable_set("@post#{@post_category.id}", Kaminari.paginate_array( @post_category.posts.order_most_viewed.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
+      else
+        instance_variable_set("@post#{@post_category.id}", Kaminari.paginate_array( @post_category.posts.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
       end
     else
       if params[:sort] == "most_reply"
         @post_categories.includes(:posts).each do | post_category |
-          instance_variable_set("@post#{post_category.id}",post_category.posts.order_most_reply.includes(:user, :replies, :post_categories, :collection_posts).page(params[:page]).per(20) )
+          instance_variable_set("@post#{post_category.id}",Kaminari.paginate_array( post_category.posts.order_most_reply.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
         end
-        @posts = Post.includes(:user, :loved_users ,:replies).order_most_reply.page(params[:page]).per(20)
+        @posts = Kaminari.paginate_array(Post.includes(:user, :loved_users , :replies).order_most_reply.select(&post_permission)).page(params[:page]).per(20)
       elsif params[:sort] == "latest_reply"
         @post_categories.includes(:posts).each do | post_category |
-          instance_variable_set("@post#{post_category.id}",post_category.posts.order_latest_reply.includes(:user, :replies, :post_categories, :collection_posts).page(params[:page]).per(20) )
+          instance_variable_set("@post#{post_category.id}", Kaminari.paginate_array( post_category.posts.order_latest_reply.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
         end
-        @posts = Post.includes(:user, :loved_users , :replies).order_latest_reply.page(params[:page]).per(20)
+        @posts = Kaminari.paginate_array(Post.includes(:user, :loved_users , :replies).order_latest_reply.select(&post_permission)).page(params[:page]).per(20)
+      elsif params[:sort] == "most_viewed"
+        @post_categories.includes(:posts).each do | post_category |
+          instance_variable_set("@post#{post_category.id}", Kaminari.paginate_array( post_category.posts.order_most_viewed.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
+        end
+        @posts = Kaminari.paginate_array(Post.includes(:user, :loved_users , :replies).order_most_viewed.select(&post_permission)).page(params[:page]).per(20)
       else
         @post_categories.includes(:posts).each do | post_category |
-          instance_variable_set("@post#{post_category.id}",post_category.posts.published.includes(:user, :replies, :post_categories, :collection_posts).page(params[:page]).per(20) )
+          instance_variable_set("@post#{post_category.id}", Kaminari.paginate_array(post_category.posts.published.includes(:user, :replies, :post_categories, :collection_posts).select(&post_permission)).page(params[:page]).per(20) )
         end
-        @posts = Post.includes(:user, :loved_users , :replies).published.page(params[:page]).per(20)
+        @posts = Kaminari.paginate_array(Post.includes(:user, :loved_users , :replies).published.select(&post_permission)).page(params[:page]).per(20)
       end
     end
   end
